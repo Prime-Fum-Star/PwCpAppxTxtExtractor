f503a68f4ef03eb7bef95e733f53680f822009b6
Prime-Fum-Star <primefuntv881@gmail.com>
2025-07-29 10:15:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "25645046"
+api_hash = "568c366b5573c5c349f0f0f25c3a4ccd"
+bot_token = "8177990799:AAFG5P-NZkNngSYuEF1WL_8F-qOgHVDBNGY"
+auth_users = [6676704302]
